a3b3d1e5c7ee0ec2594ec69f79ab97cc7bc3849d
NoToGrind <103829580+NoToGrind@users.noreply.github.com>
2024-11-21 17:33:47 +0500

    21.11

--- Labs_PSTU_2024/2.1/2.1.cpp
+++ Labs_PSTU_2024/2.1/2.1.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main()
 {
-	int n, sum = 0;
+	int n;
+    int sum = 0;
 	cin >> n;
 	if (n <= 0) cout << "Нельзя посчитать сумму" << endl;
-	for (int i = 1; i <= n; i++) sum += i;
-	cout << sum << endl;
+    else
+	    for (int i = 1; i <= n; i++) sum += i;
+	    cout << sum << endl;
 
 	return 0;
 }
